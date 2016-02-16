335436696b366a8da8a12c2c6c24a16745164e2a
Matthew Kelly <badgerati42@gmail.com>
2016-02-16 20:03:08 +0000

    Minor change to the using statements for Attributes

--- Edison.Framework/Attributes/TestFixture.cs
+++ Edison.Framework/Attributes/TestFixture.cs
@@ -7,10 +7,6 @@ License: MIT (see LICENSE for details)
  */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Edison.Framework
 {
