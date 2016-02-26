a0715bbd19b2175e2caa3894c5813a3316ffbe7b
Matthew Kelly <badgerati42@gmail.com>
2016-02-26 18:00:37 +0000

    Adds choco and nuget nuspec configs

---
+++ nuget-packages/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'Edison'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/Badgerati/Edison/releases/download/v1.0.0.6/1.0.0.6-Binaries.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+}
+
+Install-ChocolateyZipPackage @packageArgs
