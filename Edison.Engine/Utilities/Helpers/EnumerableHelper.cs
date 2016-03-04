b46a1bdeb04d10a14d91ec73037350be4a92a9c3
Matthew Kelly <badgerati42@gmail.com>
2016-03-04 20:01:23 +0000

    Resolves #17: Issues with category matching on TestFixtures

---
+++ Edison.Engine/Utilities/Helpers/EnumerableHelper.cs
@@ -0,0 +1,23 @@
+﻿/*
+Edison is designed to be simpler and more performant unit/integration testing framework.
+
+Copyright (c) 2015, Matthew Kelly (Badgerati)
+Company: Cadaeic Studios
+License: MIT (see LICENSE for details)
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Edison.Engine.Utilities.Helpers
+{
+    public static class EnumerableHelper
+    {
+
+        public static bool IsNullOrEmpty<T>(IEnumerable<T> enumerable)
+        {
+            return enumerable == default(IEnumerable<T>) || !enumerable.Any();
+        }
+
+    }
+}
